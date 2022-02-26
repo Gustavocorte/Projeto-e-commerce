8ce720df66f517e8a3ffc32b9b69fb945c723fab
Gustavo Cortes <gustavocortes981@gmail.com>
2022-02-26 10:02:49 -0300

    Tela de cadastro
    tela-cadastro

---
+++ cadastro.js
@@ -0,0 +1,21 @@
+function cadastro(){
+    var nome = document.getElementById('txtnome')
+    var ulnome = document.getElementById('txtutnome')
+    var telefone = document.getElementById('txttel')
+    var email = document.getElementById('txtemail')
+    var senha = document.getElementById('txtsenha')
+    var comsenha = document.getElementById('txtcsenha')
+    var boxter = document.getElementById('box1')
+    if (boxter[1].checked){
+        alert('Por favor veja os temos e condições')
+    } else{
+        alert('ok')
+    }
+
+
+
+
+
+
+
+}
