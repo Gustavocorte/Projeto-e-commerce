5c0af822ef0a7baad34f7207ebf741d135634b62
Gustavo Cortes <gustavocortes981@gmail.com>
2022-03-02 20:17:13 -0300

    Atualização
    Atualização

--- cadastro.js
+++ cadastro.js
@@ -1,17 +1,22 @@
-function cadastro(){
+function cadastrar(){
     var nome = document.getElementById('txtnome')
     var ulnome = document.getElementById('txtutnome')
     var telefone = document.getElementById('txttel')
     var email = document.getElementById('txtemail')
     var senha = document.getElementById('txtsenha')
     var comsenha = document.getElementById('txtcsenha')
-    var boxter = document.getElementById('box1')
-    if (boxter[1].checked){
-        alert('Por favor veja os temos e condições')
-    } else{
-        alert('ok')
-    }
+    var ter = document.getElementsByName('termos')
+    let checkbox = document.getElementById('box1')
+    let cas = document.getElementById('cadas')
+    let res = document.getElementById('res')
 
+    if(checkbox.checked) {
+        alert("ok");
+
+    } else {
+        console.log("O cliente não marcou o checkbox");
+        alert("Por favor, aceite os termos e condições");
+}
 
 
 
